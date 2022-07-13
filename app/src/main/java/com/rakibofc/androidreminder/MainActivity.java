e072acfae8aa9393e9fe8af9e0db0be144964b2b
RakibOFC <rakibull2000@gmail.com>
2022-07-13 19:54:35 +0600

    Remove button action listener

--- app/src/main/java/com/rakibofc/androidreminder/MainActivity.java
+++ app/src/main/java/com/rakibofc/androidreminder/MainActivity.java
@@ -8,12 +8,13 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button5, button10, button15;
+    public Button button5, button10, button15;
     public static String NOTIFICATION_CHANNEL_ID = "1001";
     public static String default_notification_id = "default";
     @Override
@@ -25,30 +26,31 @@ public class MainActivity extends AppCompatActivity {
         button10 = findViewById(R.id.button10);
         button15 = findViewById(R.id.button15);
 
-        button5.setOnClickListener(v->scheduleNotification(getNotification("5 seconds delay"),5000));
-        button10.setOnClickListener(v->scheduleNotification(getNotification("10 seconds delay"),10000));
-        button15.setOnClickListener(v->scheduleNotification(getNotification("15 seconds delay"),15000));
-
+        scheduleNotification(getNotification("Notification from MainActivity"));
     }
 
     //schedule notification
-    private void scheduleNotification(Notification notification, int delay){
-        Intent notificationIntent = new Intent(this,MyNotificationPublisher.class);
-        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATIONID,1);
-        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION,notification);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0, notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
-        long futureMillis = SystemClock.elapsedRealtime()+delay;
+    private void scheduleNotification(Notification notification){
+
+        Intent notificationIntent = new Intent(this, MyNotificationPublisher.class);
+
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION, notification);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        assert alarmManager !=null;
-        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureMillis, pendingIntent);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 5000, 6000, pendingIntent);
     }
 
     private Notification getNotification(String content){
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, default_notification_id);
 
         builder.setContentTitle("Schedule Notification");
         builder.setContentText(content);
-        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setAutoCancel(true);
         builder.setChannelId(NOTIFICATION_CHANNEL_ID);
 
--- app/src/main/java/com/rakibofc/androidreminder/MyNotificationPublisher.java
+++ app/src/main/java/com/rakibofc/androidreminder/MyNotificationPublisher.java
@@ -10,22 +10,25 @@ import android.os.Build;
 
 public class MyNotificationPublisher extends BroadcastReceiver {
 
-    public static String NOTIFICATIONID = "notification-id";
+    public static String NOTIFICATION_ID = "notification-id";
     public static String NOTIFICATION = "notification";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
+
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel notificationChannel = new NotificationChannel(MainActivity.NOTIFICATION_CHANNEL_ID,"NOTIFICATION_CHANNEL_NAME",importance);
             assert notificationManager !=null;
             notificationManager.createNotificationChannel(notificationChannel);
         }
 
-        int id = intent.getIntExtra(NOTIFICATIONID, 0);
-        assert notificationManager !=null;
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        assert notificationManager != null;
         notificationManager.notify(id, notification);
     }
 }
